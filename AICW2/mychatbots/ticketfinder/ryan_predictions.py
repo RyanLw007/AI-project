ec101935507b6ffc49cf9baec654aed7a14083ee
RyanLw007 <kap21tzu@uea.ac.uk>
2024-05-23 16:38:46 +0100

    cleaned historical data

--- AICW2/mychatbots/ticketfinder/ryan_predictions.py
+++ AICW2/mychatbots/ticketfinder/ryan_predictions.py
@@ -5,8 +5,10 @@ from sklearn.utils import shuffle
 from sklearn.linear_model import Ridge
 import os
 
+year = ("2017")
+
 #load dataset
-csv_directory = r'C:\Users\ryanl\Documents\Artificial Intelligence\AI project\AI-project\AICW2\mychatbots\historictraindata\LIVST_NRCH_OD_a51_2020'
+csv_directory = r'C:\Users\ryanl\Documents\Artificial Intelligence\AI project\AI-project\AICW2\mychatbots\historictraindata\LIVST_NRCH_OD_a51_' + year
 
 csv_files = [file for file in os.listdir(csv_directory) if file.endswith('.csv')]
 
@@ -22,23 +24,31 @@ for file in csv_files:
 cdf = pd.concat(dataframes, ignore_index=True)
 
 # Filter out rows where actual times are missing
-cdf = cdf.dropna(subset=['arr_at', 'dep_at'])
+cdf = cdf[cdf['wtp'].isna() | (cdf['wtp'] == '')]
 
-# Calculate delay in minutes for arrivals and departures
-cdf['arrival_delay'] = (pd.to_datetime(cdf['arr_at']) - pd.to_datetime(cdf['arr_et'])).dt.total_seconds() / 60
-cdf['departure_delay'] = (pd.to_datetime(cdf['dep_at']) - pd.to_datetime(cdf['dep_et'])).dt.total_seconds() / 60
+# convert datatype of time columns to datetime
+cdf['arr_at'] = pd.to_datetime(cdf['arr_at'], errors='coerce').dt.time
+cdf['pta'] = pd.to_datetime(cdf['pta'], errors='coerce').dt.time
+cdf['dep_at'] = pd.to_datetime(cdf['dep_at'], errors='coerce').dt.time
+cdf['ptd'] = pd.to_datetime(cdf['ptd'], errors='coerce').dt.time
 
-# Keep only relevant columns
-cdf = cdf[['tpl', 'arrival_delay', 'departure_delay']]
+# Calculate delay
+def calculate_time_difference(row):
+    if pd.notna(row['arr_at']) and pd.notna(row['pta']):
+        arr_at_time = pd.to_datetime(row['arr_at'], format='%H:%M:%S')
+        pta_time = pd.to_datetime(row['pta'], format='%H:%M:%S')
+        # Calculate the difference in seconds
+        diff_seconds = (arr_at_time - pta_time).total_seconds()
+        # Convert to hours
+        return diff_seconds / 3600
+    else:
+        return None
 
-# Rename columns for clarity
-cdf.columns = ['station_code', 'arrival_delay', 'departure_delay']
+cdf['delay'] = cdf.apply(calculate_time_difference, axis=1)
 
-# Handle missing delays (if any)
-cdf['arrival_delay'] = cdf['arrival_delay'].fillna(0)
-cdf['departure_delay'] = cdf['departure_delay'].fillna(0)
+data = cdf[['tpl','pta','ptd','arr_at','dep_at', 'delay']]
 
-cdf.to_csv('2020data.csv', index=False)
+data.to_csv(f'data{year}.csv',index=False)
 
-print(cdf.head())
+print(data.head())
 
