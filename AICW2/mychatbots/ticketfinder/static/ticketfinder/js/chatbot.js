62b967bd6f72b69286e50e05c938bb6ed76e3b01
sethstanton <sethstanton200@gmail.com>
2024-05-23 12:55:53 +0100

    adjusting front end

--- AICW2/mychatbots/ticketfinder/static/ticketfinder/js/chatbot.js
+++ AICW2/mychatbots/ticketfinder/static/ticketfinder/js/chatbot.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     function sendMessage() {
         var userInput = $("#user-input").val().trim();
         if (userInput) {
-            $("#chat-box").append("<div class='user mb-2 p-2 border rounded text-white bg-primary'>You: " + userInput + "</div>");
+            $("#chat-box").append("<div class='user mb-2 p-2 border rounded text-white bg-primary'>" + userInput + "</div>");
             $.ajax({
                 url: 'get_response/',  
                 data: {
@@ -13,14 +13,14 @@ $(document).ready(function(){
                     // this checks if data.response is an array and iterate over it
                     if (Array.isArray(data.response)) {
                         data.response.forEach(function(message) {
-                            $("#chat-box").append("<div class='bot mb-2 p-2 border rounded bg-light'>BOT: " + message + "</div>");
+                            $("#chat-box").append("<div class='bot mb-2 p-2 border rounded bg-light'>" + message + "</div>");
                         });
                     } else {
                         // if not a bunch of messages then fallback for single message responses
-                        $("#chat-box").append("<div class='bot mb-2 p-2 border rounded bg-light'>BOT: " + data.response + "</div>");
+                        $("#chat-box").append("<div class='bot mb-2 p-2 border rounded bg-light'>" + data.response + "</div>");
                     }
-                    $("#user-input").val('');  // Clear input field
-                    $("#chat-box").scrollTop($("#chat-box")[0].scrollHeight);  // Scroll to the bottom
+                    $("#user-input").val('');  
+                    $("#chat-box").scrollTop($("#chat-box")[0].scrollHeight);  
                 }
             });
         }
