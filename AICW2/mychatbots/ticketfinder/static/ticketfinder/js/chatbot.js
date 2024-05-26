87beef1fa135e18d6b02102ce9e32f40840d53d5
sethstanton <sethstanton200@gmail.com>
2024-05-26 23:54:06 +0100

    adding button functionality

--- AICW2/mychatbots/ticketfinder/static/ticketfinder/js/chatbot.js
+++ AICW2/mychatbots/ticketfinder/static/ticketfinder/js/chatbot.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
-    function sendMessage() {
-        var userInput = $("#user-input").val().trim();
+$(document).ready(function() {
+    function sendMessage(message = null) {
+        var userInput = message || $("#user-input").val().trim();
         if (userInput) {
             $("#chat-box").append("<div class='user mb-2 p-2 border rounded text-white bg-primary'>You: " + userInput + "</div>");
             $.ajax({
@@ -12,34 +12,45 @@ $(document).ready(function(){
                 success: function(data) {
                     if (Array.isArray(data.response)) {
                         data.response.forEach(function(message) {
-                            message = formatMessage(message);
-                            $("#chat-box").append("<div class='bot mb-2 p-2 border rounded bg-light'>" + message + "</div>");
+                            if (/^\d+ Station:/.test(message)) {
+                                var index = message.split(' ')[0];
+                                var buttonText = message.replace(/^\d+ Station:/, '').trim();
+                                buttonText = buttonText.replace(/\\N/g, '').trim();
+                                var button = $('<button>')
+                                    .addClass('btn station-button')
+                                    .text(index + ') ' + buttonText) 
+                                    .attr('data-index', index);
+                                $("#chat-box").append(button);
+                            } else {
+                                $("#chat-box").append("<div class='bot mb-2 p-2 border rounded bg-light'>" + message + "</div>");
+                            }
                         });
                     } else {
-                        var formattedMessage = formatMessage(data.response);
-                        $("#chat-box").append("<div class='bot mb-2 p-2 border rounded bg-light'>" + formattedMessage + "</div>");
+                        $("#chat-box").append("<div class='bot mb-2 p-2 border rounded bg-light'>" + data.response + "</div>");
                     }
                     $("#user-input").val('');  
-                    $("#chat-box").scrollTop($("#chat-box")[0].scrollHeight);  
+                    $("#chat-box").scrollTop($("#chat-box")[0].scrollHeight);
                 }
             });
         }
     }
 
-    function formatMessage(message) {
-        var urlRegex = /(https?:\/\/[^\s]+)/g;
-        return message.replace(urlRegex, function(url) {
-            return 'Please click <a href="' + url + '" target="_blank">here</a> to view this ticket';
-        });
-    }
+    // Code to handle when station button is clicked
+    $(document).on('click', '.station-button', function() {
+        var index = $(this).data('index');
+        sendMessage(index); 
+    });
+
     // Code to send message when the send button is clicked
-    $("#send").click(sendMessage);
+    $("#send").click(function() {
+        sendMessage();
+    });
+
     // Code to send message via the enter key
     $("#user-input").keypress(function(event) {
         if (event.keyCode === 13) {
-            event.preventDefault();  
+            event.preventDefault();
             sendMessage();
         }
     });
 });
-
