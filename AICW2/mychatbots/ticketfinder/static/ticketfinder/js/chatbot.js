704373b895bcff65779f95f28a525f5872d56d4a
sethstanton <sethstanton200@gmail.com>
2024-05-26 16:16:19 +0100

    Changing link presentation
    made the ticket url a hyper link instead

--- AICW2/mychatbots/ticketfinder/static/ticketfinder/js/chatbot.js
+++ AICW2/mychatbots/ticketfinder/static/ticketfinder/js/chatbot.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     function sendMessage() {
         var userInput = $("#user-input").val().trim();
         if (userInput) {
-            $("#chat-box").append("<div class='user mb-2 p-2 border rounded text-white bg-primary'>" + userInput + "</div>");
+            $("#chat-box").append("<div class='user mb-2 p-2 border rounded text-white bg-primary'>You: " + userInput + "</div>");
             $.ajax({
                 url: 'get_response/',  
                 data: {
@@ -10,14 +10,14 @@ $(document).ready(function(){
                 },
                 dataType: 'json',
                 success: function(data) {
-                    // this checks if data.response is an array and iterate over it
                     if (Array.isArray(data.response)) {
                         data.response.forEach(function(message) {
+                            message = formatMessage(message);
                             $("#chat-box").append("<div class='bot mb-2 p-2 border rounded bg-light'>" + message + "</div>");
                         });
                     } else {
-                        // if not a bunch of messages then fallback for single message responses
-                        $("#chat-box").append("<div class='bot mb-2 p-2 border rounded bg-light'>" + data.response + "</div>");
+                        var formattedMessage = formatMessage(data.response);
+                        $("#chat-box").append("<div class='bot mb-2 p-2 border rounded bg-light'>" + formattedMessage + "</div>");
                     }
                     $("#user-input").val('');  
                     $("#chat-box").scrollTop($("#chat-box")[0].scrollHeight);  
@@ -26,14 +26,20 @@ $(document).ready(function(){
         }
     }
 
+    function formatMessage(message) {
+        var urlRegex = /(https?:\/\/[^\s]+)/g;
+        return message.replace(urlRegex, function(url) {
+            return 'Please click <a href="' + url + '" target="_blank">here</a> to view this ticket';
+        });
+    }
     // Code to send message when the send button is clicked
     $("#send").click(sendMessage);
-
     // Code to send message via the enter key
     $("#user-input").keypress(function(event) {
-        if (event.keyCode === 13) {  
+        if (event.keyCode === 13) {
             event.preventDefault();  
             sendMessage();
         }
     });
 });
+
