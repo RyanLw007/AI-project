5d07cd0eda88009b29a6d6e0316c40aa0e0f4b52
sethstanton <sethstanton200@gmail.com>
2024-05-28 12:15:12 +0100

    cleaning up

--- AICW2/mychatbots/ticketfinder/urls.py
+++ AICW2/mychatbots/ticketfinder/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views#, NLP2
+from . import views
 from ticketfinder.views import clear_json
 
 
@@ -10,5 +10,5 @@ urlpatterns = [
     path('get_response/', views.get_response, name='get_response'),
     path('results/', views.train_view, name='train_results'),
     path('clear_json/', views.clear_json, name='clear_json'),
-    #path('get_response/',NLP2, name='get_response')
+    
 ]
